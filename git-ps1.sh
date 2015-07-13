b629af10c678256f395db10cf7e5b7e2feac3927
Daniel Murphy <dmurph@chromium.org>
2015-07-13 02:29:22 -0700

    added path shortener

--- git-ps1.sh
+++ git-ps1.sh
@@ -1,7 +1,17 @@
 #!bin/bash
 
+declare -A GIT_PS1_PATH_SHORTENER
+
+function extra_shorten() {
+  out=$1
+  for i in "${!GIT_PS1_PATH_SHORTENER[@]}"
+  do
+    out="${out/$i/${GIT_PS1_PATH_SHORTENER[$i]}}"
+  done
+  echo $out
+}
 
 if [ -f /etc/bash_completion.d/git5 ]; then
   source /etc/bash_completion.d/git5
-  export PS1='${debian_chroot:+($debian_chroot)}\A \[\e[0;35m\]\u \[\e[01;32m\]$(__git_ps1 "[%s]") \[\e[01;36m\]\w\[\e[01;00m\]$\[\e[00m\] '
+  export PS1='${debian_chroot:+($debian_chroot)}\A \[\e[0;35m\]$(test "\u" != "dmurph" && echo "\u ")\[\e[01;32m\]$(__git_ps1 "[%s]") \[\e[01;36m\]$(extra_shorten "\w")\[\e[01;00m\]$\[\e[00m\] '
 fi
